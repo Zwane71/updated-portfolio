d642e0e86babe702d44b585f23362e9d6f96b31e
MakZwane71 <makhohlisathemba@gmail.com>
2025-01-27 17:04:38 +0200

    adding vercel analytics

--- .next/static/chunks/app_layout_61af54.js
+++ .next/static/chunks/app_layout_61af54.js
@@ -6,7 +6,7 @@
   "path": "static/chunks/app_layout_61af54.js",
   "chunks": [
     "static/chunks/[root of the server]__b537f8._.css",
-    "static/chunks/app_layout_d9f7ea.js"
+    "static/chunks/_490b61._.js"
   ],
   "source": "dynamic"
 });
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Outfit, Ovo } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const outfit = Outfit({
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
 		<html lang='en' className="scroll-smooth  ">
 			<body className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
